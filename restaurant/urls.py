bf88427c2786c05ffd48d4e42801dc8e5549eff0
ViniciusBatestin <viniciusbatestin@gmail.com>
2025-04-07 20:33:49 +0200

    Menu Item / Single Menu Item display added

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>', views.SingleMenuItem.as_view())
+
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework.response import Response
+
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django.contrib.auth.models import User, Group
+
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# handles post and get
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    # def get_permissions(self):
+    #     permission_classes = []
+    #     if self.request.method != 'GET':
+    #         permission_classes = [IsAuthenticated]
+    #     return Response(permission() for permission in permission_classes)
+
+#handles get, put and delete
+class SingleMenuItem(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    # def get_permissions(self):
+    #     permission_class = []
+    #     if self.request.method != 'GET':
+    #         permission_class = [IsAuthenticated]
+    #     return Response(permission() for permission in permission_class)

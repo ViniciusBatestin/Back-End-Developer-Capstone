30db738619b4d1df5bc2a2b051ff72b1cb904d66
ViniciusBatestin <viniciusbatestin@gmail.com>
2025-04-07 19:22:09 +0200

    models creation

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a)ll(++x+4l-3^+%26v^oq#r_712j7zb%3#3yc7qgo3j_a%ur7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
